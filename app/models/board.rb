dc9db28758da78c928c84c282b11ce8baa92f71c
nao1234 <ainanmikan@gmail.com>
2023-04-24 23:17:54 +0900

    日本語化

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,32 @@
 class Board < ApplicationRecord
+  validates :title, presence: true
+  validates :title, length: {minimum: 2, maximum: 100}
+  validates :title, format: {with: /\A(?!\@)/ }
+
+  validates :content, presence: true
+  validates :content, length: {minimum: 10}
+  validates :content, uniqueness: true
+
+  validate :validate_title_and_content_length
+
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+
+  def display_created_at
+      I18n.l(self.created_at, format: :default)
+  end
+
+  def like_count
+      likes.count
+  end
+
+  def auther_name
+      user.display_name
+  end
+
+  private
+  def validate_title_and_content_length
+      char_count = self.title.length + self.content.length
+      errors.add(:content, '20文字以上で!') unless char_count > 20
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  has_many :boardss, dependent: :destroy
+  has_one :profile, dependent: :destroy
+       
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
+      
+  def has_written?(board)
+    boards.exists?(id: board.id)
+  end
+
+  def display_name
+      profile&.nickname || self.email.split('@').first
+  end
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
+  end
 end
