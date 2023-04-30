d88d43c7bcf1a5df29ddb7cd97a06e61c956e4ba
nao1234 <ainanmikan@gmail.com>
2023-04-30 11:46:37 +0900

    application内でのbeforeアクションの削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,15 +3,13 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
-  belongs_to :board   
+  belongs_to :board
   belongs_to :user
-  
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
 
-  def auther_name
-    user.display_name
-  end
-  
+
+
 end
